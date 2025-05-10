db0cf2600fac73aa81b2829387af61c5f88fceca
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2025-05-10 10:20:24 -0300

    Adicionando comentários no arquivo .bat

--- k8/stopall.bat
+++ k8/stopall.bat
@@ -1,8 +1,8 @@
-# 1. Excluir todos os recursos do Kubernetes (pods, services, deployments, etc)
+REM # 1. Excluir todos os recursos do Kubernetes (pods, services, deployments, etc)
 kubectl delete all --all
 
-# 2. Parar o Minikube
+REM # 2. Parar o Minikube
 minikube stop
 
-# 3. Se quiser realmente "zerar" tudo, você pode excluir o cluster Minikube
+REM #3. Se quiser realmente "zerar" tudo, você pode excluir o cluster Minikube
 minikube delete
