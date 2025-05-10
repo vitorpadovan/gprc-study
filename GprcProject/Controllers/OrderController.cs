0e170bfde224dfe468018866d416f52ea23e4047
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2025-05-10 10:01:42 -0300

    Adicionando controllers para executar o projeto

---
+++ GprcProject/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using GprcProject.Controllers.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GprcProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public void SaveOrder([FromBody] IterationRequest iterationRequest)
+        {
+            _logger.LogInformation("O que estamos recebendo é {dd}",iterationRequest);
+        }
+    }
+}
---
+++ GprcProject/Controllers/Request/IterationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace GprcProject.Controllers.Request
+{
+    public class IterationRequest
+    {
+        public int Usuario { get; set; }
+        public int Iteracao { get; set; }
+
+        public override string? ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
